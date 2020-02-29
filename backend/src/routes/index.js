0d95e87059de6209d9ca743d56779cb1336fd789
Tommy Carrascal <Carrascalx@gmail.com>
2020-02-29 16:17:44 -0500

    first rough draft of profile management backend

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const book = require('../controllers/book');
+const user = require('../controllers/user');
 
 const apiRouter = Router();
 
@@ -9,5 +10,10 @@ apiRouter.get('/', (req, res) => res.send('ayy group 5'));
 /* ------ Book Routes ------ */
 apiRouter.get('/books', book.read);
 apiRouter.post('/books', book.create);
+
+/* ------ User Routes ------ */
+apiRouter.get('/user', user.read);
+apiRouter.post('/user', user.create);
+apiRouter.update('/user', user.update);
 
 module.exports = { apiRouter };
