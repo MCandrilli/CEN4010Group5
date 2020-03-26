d0d909e0e6133fe98059a5d267430f908fc8625f
Caitlin Brown <caity3398@gmail.com>
2020-03-25 20:28:10 -0400

    New branch with integrationof master and updatedShoppingCart branch.

---
+++ frontend/src/components/ShoppingCart/shoppingCartStyles.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+import { withStyles} from '@material-ui/core/styles';
+import { Tooltip } from '@material-ui/core';
+
+const StyledCoverArt = styled.img`height: 150px;`;
+
+const StyledDeleteButton = styled.button`
+	background: transparent;
+	border: none;
+`;
+
+const StyledActionButton = styled.button`
+	background: transparent;
+	border-color: blue;
+	border-width: thin;
+	color: blue;
+	text-decoration: underline;
+`;
+
+const StyledCheckoutButton = styled.button`
+	background: darkblue;
+	color: white;
+	border: outset;
+	border-radius: 12px;
+	padding: 5px 10px;
+	font-variant-caps: all-petite-caps;
+	font-size: 30px;
+	font-weight: lighter;
+	margin-left: 770px;
+	margin-top: 50px;
+`;
+
+const StyledBookTitle = styled.text`white-space: normal;`;
+
+const StyledShoppingCartTitle = styled.h3`
+	font-variant: all-petite-caps;
+	text-decoration: overline;
+	color: dimgrey;
+	font-size: 40px;
+	font-weight: lighter;
+	padding-top: 10px;
+`;
+
+const StyledSFLTitle = styled.h3`
+	font-variant: all-petite-caps;
+	text-decoration: overline;
+	color: dimgrey;
+	font-size: 28px;
+	font-weight: lighter;
+`;
+
+const StyledSubtotal = styled.p`
+	color: rgba(0, 0, 0, 0.54);
+	font-size: 20px;
+	text-transform: uppercase;
+	margin: unset;
+`;
+
+const StyledTooltip = withStyles(() =>({
+	tooltip: {
+		maxWidth: 210,
+		fontSize: 12
+	}
+}))(Tooltip)
+
+export {
+	StyledCoverArt,
+	StyledDeleteButton,
+	StyledActionButton,
+	StyledBookTitle,
+	StyledShoppingCartTitle,
+	StyledSFLTitle,
+	StyledSubtotal,
+	StyledCheckoutButton,
+	StyledTooltip
+};
--- frontend/src/components/shoppingcart.js
+++ frontend/src/components/shoppingcart.js
@@ -1,220 +1,215 @@
 import React, { Component, useState } from 'react';
-import {DataTable, TableHeader, Textfield} from 'react-mdl';
-import styled from "styled-components";
-import delete_logo from "./images/delete_bin.png"
+import { DataTable, TableHeader, Textfield } from 'react-mdl';
+import delete_logo from './images/delete_bin.png';
 import Tooltip from '@material-ui/core/Tooltip';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
-const StyledDiv = styled.div`
-    margin-left: 400px;`;
+import {
+	StyledCoverArt,
+	StyledDeleteButton,
+	StyledActionButton,
+	StyledBookTitle,
+	StyledShoppingCartTitle,
+	StyledSFLTitle,
+	StyledSubtotal,
+	StyledCheckoutButton,
+	StyledTooltip
+} from './ShoppingCart/shoppingCartStyles';
+import {
+	img_url_prefix,
+	addToCart,
+	getSubtotal,
+	saveForLater,
+	removeFromCart,
+	removeFromSFL,
+	backToCart,
+	updateQuantity
+} from './ShoppingCart/useShoppingCart';
+import {
+	cart,
+	save_for_later,
+	updateShoppingCart,
+	updateSFL,
+	updateLocalStorage
+} from './ShoppingCart/shoppingCartStorage';
 
-const StyledCoverArt = styled.img`
-    height: 150px;`;
+class ShoppingCart extends Component {
+	constructor() {
+		super();
+		this.state = {
+			click_toggle_on: true,
+			quantity_toggles: this.initQuantityToggles(cart.length)
+		};
+	}
 
-const StyledDeleteButton = styled.button`
-    background: transparent;
-    border: none;`;
+	/* Direct all button clicks to the proper functions and update changes to local storage */
+	handleClick = (id, action) => {
+		this.setState((prevState) => ({ click_toggle_on: !prevState.click_toggle_on }));
+		action === 'delete_cart'
+			? removeFromCart(id)
+			: action === 'save_for_later'
+				? saveForLater(id)
+				: action === 'delete_SFL' ? removeFromSFL(id) : backToCart(id);
+		updateLocalStorage();
+	};
 
-const StyledActionButton = styled.button`
-    background: transparent;
-    border-color: blue;
-    border-width: thin;
-    color: blue;
-    text-decoration: underline;`;
+	displayDeleteButton = (id, action) => {
+		return (
+			<StyledTooltip title="Delete">
+				<StyledDeleteButton
+					onClick={() => {
+						this.handleClick(id, action);
+					}}
+				>
+					<img src={delete_logo} alt="delete logo" style={{ height: '30px' }} />
+				</StyledDeleteButton>
+			</StyledTooltip>
+		);
+	};
 
-const StyledBookTite = styled.text`
-    white-space: normal;`;
+	initQuantityToggles = (num_items) => {
+		let new_toggles = [];
+		for (let i = 0; i < num_items; i++) new_toggles.push(true);
+		return new_toggles;
+	};
 
-const StyledShoppingCartTitle = styled.h3`
-    font-variant: all-petite-caps;
-    text-decoration: overline;
-    color: dimgrey;
-    font-size: 40px;
-    font-weight: lighter;
-    padding-top: 10px;`;
+	toggleQuantity = (item, value) => {
+		let new_toggles = this.state.quantity_toggles;
+		new_toggles[item] = value;
+		this.setState({ quantity_toggles: new_toggles });
+	};
 
-const StyledSFLTitle = styled.h3`
-    font-variant: all-petite-caps;
-    text-decoration: overline;
-    color: dimgrey;
-    font-size: 28px;
-    font-weight: lighter;`;
+	handleQuantityChange = (e, id) => {
+		const { value } = e.target;
+		let numerical_value = value - 0;
+		if (numerical_value > 0 && numerical_value % 1 === 0) {
+			updateQuantity(id, numerical_value);
+		}
+	};
 
-const StyledSubtotal = styled.p`
-    color: rgba(0, 0, 0, 0.54);
-    font-size: 20px;
-    text-transform: uppercase;
-    margin: unset;`;
+	quantityField = (id, index) => {
+		return (
+			<ClickAwayListener
+				onClickAway={() => {
+					this.toggleQuantity(index, true);
+				}}
+			><StyledTooltip title="Quantity should only contain integer numbers from 1-9999.">
+				<Textfield
+					onChange={(e) => {
+						this.handleQuantityChange(e, id);
+					}}
+					onKeyPress={(e) => {e.key === "Enter" && this.toggleQuantity(index, true)}}
+					pattern="[0-9]{1,4}"
+					maxLength = "4"
+					error="invalid input"
+					label="..."
+					style={{ width: '30px' }}
+				/>
+			</StyledTooltip></ClickAwayListener>
+		);
+	};
 
-const cart = [];
-const save_for_later = [];
-const img_url_prefix = 'https://raw.githubusercontent.com/benoitvallon/100-best-books/master/static/';
+	createCart = () => {
+		let items = cart.map((book, index) => ({
+			cover_art: <StyledCoverArt src={img_url_prefix + book.img_link} alt={book.title + ' cover art'} />,
+			booktitle: <StyledBookTitle>{book.title}</StyledBookTitle>,
+			quantity: (
+				<p1 onClick={() => this.toggleQuantity(index, false)}>
+					{this.state.quantity_toggles[index] === false ? this.quantityField(book.id, index) : book.quantity}
+				</p1>
+			),
+			price: '$' + book.price,
+			delete: this.displayDeleteButton(book.id, 'delete_cart'),
+			save_for_later: (
+				<StyledActionButton onClick={() => this.handleClick(book.id, 'save_for_later')}>
+					Save For Later
+				</StyledActionButton>
+			)
+		}));
+		let subtotal = {
+			cover_art: '',
+			booktitle: '',
+			quantity: '',
+			price: '',
+			delete: getSubtotal(),
+			save_for_later: <StyledSubtotal>Subtotal: </StyledSubtotal>
+		};
+		items.push(subtotal);
 
-/* Add a book to the shopping cart */
-const addToCart = book =>
-{   
-    //if (book == null){return;}
-    let duplicate = findBookInCartByIndex(book._id);           // Find index of possible duplicate
-    if (duplicate == -1)        // No duplicate present
-    {
-        cart.push({id: book._id, img_link: book.imageLink, title: book.title, quantity: 1, price: book.price});
-    }
-    else                        // Duplicate present
-    {
-        cart[duplicate].quantity++;
-    }
-    console.log(cart);
-}
+		return (
+			<DataTable shadow={0} style={{ width: '800px', fontSize: '16px' }} rows={items}>
+				<TableHeader name="cover_art" style={{ color: 'transparent' }}>
+					Cover Art
+				</TableHeader>
+				<TableHeader name="booktitle" style={{ fontSize: '16px', minWidth: '350px' }}>
+					Book Title
+				</TableHeader>
+				<TableHeader numeric name="quantity" style={{ fontSize: '16px' }}>
+					Quantity
+				</TableHeader>
+				<TableHeader numeric name="price" style={{ fontSize: '16px' }}>
+					Price
+				</TableHeader>
+				<TableHeader name="save_for_later" style={{ color: 'transparent' }}>
+					SFL
+				</TableHeader>
+				<TableHeader name="delete" style={{ color: 'transparent' }}>
+					Delete
+				</TableHeader>
+			</DataTable>
+		);
+	};
 
-/* Calculate and update cart's subtotal */
-const getSubtotal = () =>     
-{
-    let newTotal = 0;
-    for (const [index, book] of cart.entries())
-    {
-        newTotal += (book.price * book.quantity);
-    }
-    return <StyledSubtotal>${newTotal.toFixed(2)}</StyledSubtotal>;
-}
+	createSFL = () => {
+		return (
+			<DataTable
+				shadow={0}
+				style={{ width: '650px', fontSize: '16px' }}
+				rows={save_for_later.map((book, index) => ({
+					cover_art: <StyledCoverArt src={img_url_prefix + book.img_link} alt={book.title + ' cover art'} />,
+					booktitle: <StyledBookTitle>{book.title}</StyledBookTitle>,
+					price: book.price,
+					delete: this.displayDeleteButton(book.id, 'delete_SFL'),
+					add_to_cart: (
+						<StyledActionButton onClick={() => this.handleClick(book.id, 'back_to_cart')}>
+							Add To Cart
+						</StyledActionButton>
+					)
+				}))}
+			>
+				<TableHeader name="cover_art" style={{ color: 'transparent' }}>
+					Cover Art
+				</TableHeader>
+				<TableHeader name="booktitle" style={{ fontSize: '16px', minWidth: '350px' }}>
+					Book Title
+				</TableHeader>
+				<TableHeader numeric name="price" style={{ fontSize: '16px' }}>
+					Price
+				</TableHeader>
+				<TableHeader name="add_to_cart" style={{ color: 'transparent' }}>
+					BTC
+				</TableHeader>
+				<TableHeader name="delete" style={{ color: 'transparent' }}>
+					Delete
+				</TableHeader>
+			</DataTable>
+		);
+	};
 
-/* 
-    Find book in cart by book id and return the book's index if the id is found.
-    Otherwise, return -1
-*/
-const findBookInCartByIndex = id =>
-{
-    return cart.findIndex(book => book.id == id);
-}
-
-const findBookInSFLByIndex = id =>
-{
-    return save_for_later.findIndex(book => book.id == id);
-}
-
-const saveForLater = id =>
-{
-    let index = findBookInCartByIndex(id);
-    if (index != -1)
-    {
-        save_for_later.push(cart[index]);
-        cart.splice(index, 1);
-    }
-}
-
-const removeFromCart = id =>
-{
-    let index = findBookInCartByIndex(id);
-    if (index != -1)
-    {
-        cart.splice(index, 1);
-    }
-}
-
-const removeFromSFL = id =>
-{
-    let index = findBookInSFLByIndex(id);
-    if (index != -1)
-    {
-        save_for_later.splice(index, 1);
-    }
-}
-
-const backToCart = id =>
-{
-    let sfl_index = findBookInSFLByIndex(id);
-    if (sfl_index != -1)
-    {
-        let newBook = save_for_later[sfl_index];
-        newBook.quantity = 1;
-        save_for_later.splice(sfl_index, 1);
-
-        let cart_index = findBookInCartByIndex(id);
-        (cart_index != -1) ? cart[cart_index].quantity++ : cart.push(newBook);  
-    }
-}
-
-class ShoppingCart extends Component
-{
-    constructor(){
-        super();
-        this.state = {
-            delete_toggle_on: true,
-            save_for_later_toggle_on: true
-        }
-    }
-
-    handleClick = (id, action) =>
-    {
-        this.setState(prevState => ({delete_toggle_on: !prevState.delete_toggle_on}));
-        (action == "delete_cart") ? removeFromCart(id) : 
-        (action == "save_for_later") ? saveForLater(id) : 
-        (action == "delete_SFL") ? removeFromSFL(id) : backToCart(id);
-    }
-
-    displayDeleteButton = (id, action) =>
-    {
-        return( 
-            <Tooltip title="Delete">
-                <StyledDeleteButton onClick={() => {this.handleClick(id, action)}}>
-                    <img src={delete_logo} alt="delete logo" style={{height: "30px"}} />
-                </StyledDeleteButton>
-            </Tooltip>);
-    }
-
-    createCart = () => 
-    {
-        let items =  cart.map((book, index) => ({cover_art: <StyledCoverArt src = {img_url_prefix + book.img_link} alt = {book.title + " cover art"} />,
-                     booktitle: <StyledBookTite>{book.title}</StyledBookTite>, quantity: book.quantity, price: "$" + book.price, 
-                     delete: this.displayDeleteButton(book.id, "delete_cart"), 
-                     save_for_later: <StyledActionButton onClick={() => this.handleClick(book.id, "save_for_later")}>Save For Later</StyledActionButton>}));
-        let subtotal = {cover_art: "", booktitle: "", quantity: "", price: "", delete: getSubtotal(), save_for_later: <StyledSubtotal>Subtotal: </StyledSubtotal>};
-        items.push(subtotal);
-
-        return (
-        <DataTable
-            shadow={0}
-            style={{width: '800px', fontSize: '16px'}}
-            rows = {items}
-        >
-            <TableHeader name="cover_art" style={{color: "transparent"}}>Cover Art</TableHeader>
-            <TableHeader name="booktitle" style={{fontSize: "16px", minWidth: "350px"}}>Book Title</TableHeader>
-            <TableHeader numeric name="quantity" style={{fontSize: "16px"}}>Quantity</TableHeader>
-            <TableHeader numeric name="price" style={{fontSize: "16px"}}>Price</TableHeader>
-            <TableHeader name="save_for_later" style={{color: "transparent"}}>SFL</TableHeader>
-            <TableHeader name="delete" style={{color: "transparent"}}>Delete</TableHeader>
-        </DataTable>
-        );
-    }
-
-    createSFL = () =>
-    {
-        return(
-        <DataTable
-            shadow={0}
-            style={{width: '650px', fontSize: '16px'}}
-            rows = {save_for_later.map((book, index) => ({cover_art: <StyledCoverArt src = {img_url_prefix + book.img_link} alt = {book.title + " cover art"} />,
-            booktitle: <StyledBookTite>{book.title}</StyledBookTite>, price: book.price, 
-            delete: this.displayDeleteButton(book.id, "delete_SFL"), 
-            add_to_cart: <StyledActionButton onClick={() => this.handleClick(book.id, "back_to_cart")}>Add To Cart</StyledActionButton>}))}
-        >
-            <TableHeader name="cover_art" style={{color: "transparent"}}>Cover Art</TableHeader>
-            <TableHeader name="booktitle" style={{fontSize: "16px", minWidth: "350px"}}>Book Title</TableHeader>
-            <TableHeader numeric name="price" style={{fontSize: "16px"}}>Price</TableHeader>
-            <TableHeader name="add_to_cart" style={{color: "transparent"}}>BTC</TableHeader>
-            <TableHeader name="delete" style={{color: "transparent"}}>Delete</TableHeader>
-        </DataTable>
-        );
-    }
-
-    render() {
-    return(
-        <div style={{padding: '5px 350px'}}>
-         <StyledShoppingCartTitle>Shopping Cart</StyledShoppingCartTitle>
-        {(cart.length == 0) ? <p1 style={{fontSize: "20px"}}>Your cart is empty.</p1> : <this.createCart/>}
-        {(save_for_later.length > 0) && <StyledSFLTitle>Save For Later</StyledSFLTitle>}
-        {(save_for_later.length > 0) && <this.createSFL/>}
-        </div>
-    )}
+	render() {
+		updateShoppingCart();
+		updateSFL();
+		return (
+			<div style={{ padding: '5px 350px' }}>
+				<StyledShoppingCartTitle>Shopping Cart</StyledShoppingCartTitle>
+				{cart.length === 0 ? <p1 style={{ fontSize: '20px' }}>Your cart is empty.</p1> : <this.createCart />}
+				{save_for_later.length > 0 && <StyledSFLTitle>Save For Later</StyledSFLTitle>}
+				{save_for_later.length > 0 && <this.createSFL />}
+				{cart.length > 0 && <StyledCheckoutButton>Checkout</StyledCheckoutButton>}
+			</div>
+		);
+	}
 }
 
 export default ShoppingCart;
-export {addToCart};
+export { addToCart };
