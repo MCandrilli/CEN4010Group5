bee548d74c18f77d0e58ed3ab35b77f96ce03d22
Manny Castaneda <Ecast221@gmail.com>
2020-03-28 13:39:32 -0400

    Fixed wishlist move items

--- backend/src/models/wishlistItem.js
+++ backend/src/models/wishlistItem.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const wishListItemSchema = new mongoose.Schema({
+	
 	title: { type: String, required: true },
 	belongsTo: { type: String, required: true },
 	imageLink: { type: String, required: true },
-	price: { type: Number, required: true },
-	id: { type: String, required: true }
+	price: { type: Number, required: true }
 });
 
 const wishListItem = mongoose.model('WishListItem', wishListItemSchema);
